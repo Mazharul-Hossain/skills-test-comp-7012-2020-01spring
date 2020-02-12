1ab3d7bbff6dfe7e41db33e317c7839570a9f423
Mazharul Hossain <2561458+m-h@users.noreply.github.com>
2020-02-12 13:16:35 -0600

    Completed comp7012-s03

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,25 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+a1 = Article.create!(author: "John Maloney, Mitchel Resnick, Natalie Rusk, Brian Silverman, and Evelyn Eastmond",
+ journal: "ACM Transactions on Computing Education (TOCE)",
+ title: "The Scratch Programming Language and Environment",
+ year: 2010  )   
+
+a2 = Article.create!(author:  "Juha Sorva, Ville Karavirta, and Lauri Malmi	", 
+ journal: "ACM Transactions on Computing Education (TOCE)	",
+ title: "A Review of Generic Program Visualization Systems for Introductory Programming Education	",
+ year: 2013  )
+
+a3 = Article.create!(author: "Christopher Douce, David Livingstone, and James Orwell	",
+ journal: "Journal on Educational Resources in Computing (JERIC)	",
+ title: "Automatic test-based assessment of programming: A review	",
+ year:   2005
+)
+
+a4 = Article.create!(author: "Frank L. Greitzer, Olga Anna Kuchar, and Kristy Huston	 ",
+ journal:  "Journal on Educational Resources in Computing (JERIC)	",
+ title: "Cognitive science implications for enhancing training effectiveness in a serious gaming context	",
+ year:  2007
+)
