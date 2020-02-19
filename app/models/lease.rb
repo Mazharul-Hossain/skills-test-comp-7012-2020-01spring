77541c189111b1dfe26c318b2d58bdf0a993d013
Mazharul Hossain <2561458+m-h@users.noreply.github.com>
2020-02-19 13:29:17 -0600

    Completed comp7012-s04

--- app/models/lease.rb
+++ app/models/lease.rb
@@ -13,11 +13,13 @@
 
 class Lease < ApplicationRecord
     validates :tenant, presence: true
+    validates :deposit, presence: true
+    validates :monthly_rent, presence: true
 
     validate :deposit_cannot_be_more_than_two_times_the_monthly_rent
 
     def deposit_cannot_be_more_than_two_times_the_monthly_rent
-        if deposit > monthly_rent * 2
+        if deposit && monthly_rent && deposit > monthly_rent * 2
             errors.add(:deposit, "deposit cannot be more than two times the monthly rent")
         end
     end
